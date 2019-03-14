a3f821d4d5d9be498290a3740d34a0d0303667dd
saswata-dutta <saswat.dutta@gmail.com>
2019-03-15 01:43:02 +0530

    adds wrapper to ease json parse and eval
    helps avoid unchecked assignment of parsedExp in java

---
+++ src/main/scala/org/saswata/expressions/Evaluate.scala
@@ -0,0 +1,25 @@
+package org.saswata.expressions
+
+import org.saswata.expressions.Expression.Exp
+
+object Evaluate {
+  def fromJsonAsBool(env: Map[String, Any], json: String): Boolean = {
+    val exp: Exp[Boolean] = JsonParser.parseBoolExp(json)
+    new Expression(env).eval[Boolean](exp)
+  }
+
+  def fromJsonAsBool(jmap: java.util.Map[java.lang.String, java.lang.Object], json: String): Boolean = {
+    val exp: Exp[Boolean] = JsonParser.parseBoolExp(json)
+    new Expression(jmap).eval[Boolean](exp)
+  }
+
+  def fromJsonAsNumber(env: Map[String, Any], json: String): Double = {
+    val exp: Exp[Double] = JsonParser.parseNumExp(json)
+    new Expression(env).eval[Double](exp)
+  }
+
+  def fromJsonAsNumber(jmap: java.util.Map[java.lang.String, java.lang.Object], json: String): Double = {
+    val exp: Exp[Double] = JsonParser.parseNumExp(json)
+    new Expression(jmap).eval[Double](exp)
+  }
+}
