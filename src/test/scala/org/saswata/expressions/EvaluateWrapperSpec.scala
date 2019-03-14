573b328ea3cae918fcd481e24dcc0276b5e122d5
saswata-dutta <saswat.dutta@gmail.com>
2019-03-15 01:43:02 +0530

    adds wrapper to ease json parse and eval
    helps avoid unchecked assignment of parsedExp in java

---
+++ src/test/scala/org/saswata/expressions/EvaluateWrapperSpec.scala
@@ -0,0 +1,55 @@
+package org.saswata.expressions
+
+import java.util
+
+import org.scalatest._
+
+class EvaluateWrapperSpec extends FlatSpec with Matchers {
+  "The Evaluator Wrapper" should "evaluate numeric expressions" in {
+    val json =
+      """
+        |{
+        |  "type": "NUM_SYMBOL",
+        |  "key": "num"
+        |}
+      """.stripMargin
+
+    Evaluate.fromJsonAsNumber(Map.empty[String, Any], json) shouldEqual 0
+    Evaluate.fromJsonAsNumber(Map("num" -> 1), json) shouldEqual 1.0
+    Evaluate.fromJsonAsNumber(Map("num" -> "2"), json) shouldEqual 2.0
+
+    val jmap: java.util.Map[java.lang.String, java.lang.Object] = new util.HashMap()
+    Evaluate.fromJsonAsNumber(jmap, json) shouldEqual 0
+    jmap.put("num", Integer.valueOf(1))
+    Evaluate.fromJsonAsNumber(jmap, json) shouldEqual 1.0
+    jmap.put("num", "2")
+    Evaluate.fromJsonAsNumber(jmap, json) shouldEqual 2.0
+  }
+
+  it should "evaluate bool expressions" in {
+    val json =
+      """
+        |{
+        |  "type": "BOOL_SYMBOL",
+        |  "key": "flag"
+        |}
+      """.stripMargin
+
+    Evaluate.fromJsonAsBool(Map.empty[String, Any], json) shouldEqual false
+    Evaluate.fromJsonAsBool(Map("num" -> 1), json) shouldEqual false
+    Evaluate.fromJsonAsBool(Map("flag" -> false), json) shouldEqual false
+    Evaluate.fromJsonAsBool(Map("flag" -> true), json) shouldEqual true
+    Evaluate.fromJsonAsBool(Map("flag" -> "true"), json) shouldEqual true
+
+    val jmap: java.util.Map[java.lang.String, java.lang.Object] = new util.HashMap()
+    Evaluate.fromJsonAsBool(jmap, json) shouldEqual false
+    jmap.put("num", "2")
+    Evaluate.fromJsonAsBool(jmap, json) shouldEqual false
+    jmap.put("flag", "true")
+    Evaluate.fromJsonAsBool(jmap, json) shouldEqual true
+    jmap.put("flag", "false")
+    Evaluate.fromJsonAsBool(jmap, json) shouldEqual false
+    jmap.put("flag", java.lang.Boolean.TRUE)
+    Evaluate.fromJsonAsBool(jmap, json) shouldEqual true
+  }
+}
